9a9e7fddeff69dad76292dae343ff45f2ea3c15f
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-05-13 01:07:06 +0530

    solved april long b

---
+++ Template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+    long long int mod=1000000007;
+	cin >> t;
+    while(t--)
+    {
+        long long int n,sum=0;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        for (int i=0;i<n;i++)
+        {
+            sum+=max(arr.back()-i,0);
+            arr.pop_back();
+        }
+        cout<<sum%mod<<endl;
+    }
+    return 0;
+}
