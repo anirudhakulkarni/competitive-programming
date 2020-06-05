76c0da50ac937f7208669ec3e26b9d19ba3765e6
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-06-05 14:38:32 +0530

    .

---
+++ codeforces/647D2/B_Johnny_and_His_Hobbies.py
@@ -0,0 +1,9 @@
+import math
+# cook your dish here
+for t in range(int(input())):
+    n=int(input())
+    #a,b=map(int,input().split())
+    arr=list(map(int,input().split()))
+    a=max(arr)
+    for i in range(1,a+1):
+        
