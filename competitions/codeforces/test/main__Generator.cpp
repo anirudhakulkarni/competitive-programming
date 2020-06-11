126774701909efbdc5dda96dc4b98d1145edf37e
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-06-11 14:35:41 +0530

    rearranged testcases and removed stray files

---
+++ competitions/codeforces/test/main__Generator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+        cout<<rand()<<" "<<rand()<<endl;
+        }
+
--- practice/graphs/main.cpp
+++ practice/graphs/main.cpp
@@ -1,21 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-
-
-
+typedef long long LL;
 typedef vector<int> vi; 
 typedef vector< vi > vvi; 
-
+/////////////////////////
 #define F first
 #define S second
 #define PB push_back
 #define MP make_pair
-#define loop(i,a,b) for (int i = a; i <= b; i++)
-
-
-
+#define loop(i,a,b) for (int i = a; i < b; i++)
+//////////////////////////
 #define printvector(n) for(int i=0;i<n.size();i++){cout<<n[i]<<" ";}cout<<'\n'
 #define printstack(n) for(int i=0;i<n.size();i++){cout<<n[i]<<" ";}cout<<'\n'
 #define PI_val 3.14159265359
@@ -24,28 +18,18 @@ typedef vector< vi > vvi;
 #define fio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define endl "\n"
 long long int mod=1000000007;
+//////////////////////////
 
 int main()
 {
-    fio;
-	ll t;
-	cin >> t;
-    while(t--)
-    {
-        long long int n,sum=0;
-        cin>>n;
-        vector<int> arr(n);
-        for(int i=0;i<n;i++)
-        {
-            cin>>arr[i];
-        }
-        
- //      printvector(arr);
-//        sort(arr.begin(),arr.end());
-
-
-    }
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("E:/codes/testcases/input.in", "r", stdin);
+    // for writing output to output.txt
+    freopen("E:/codes/testcases/output.in", "w", stdout);
+    #endif
+    LL t;
+    cin >> t;
+    
     return 0;
 }
-
-
