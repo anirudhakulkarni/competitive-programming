7ed0fbb38a3c8ad364d952aade214816fad96f2e
ANIRUDHA KULKARNI <kulkarnianirudha8@gmail.com>
2020-07-19 13:06:10 +0530

    #656

--- competitions/codechef/COLE2020/A.cpp
+++ competitions/codechef/COLE2020/A.cpp
@@ -8,22 +8,31 @@ int main()
     cin >> t;
     while(t--)
     {
-        long long n,ans=0;
+       long long n,q;
+      
         cin>>n;
         vector<long long> arr(n);
-        for(long long i=0;i<n;i++)
+        for(int i=0;i<n;i++)
         {
             cin>>arr[i];
-            if(arr[i]%2==0){
-                ans=1;
-
+        }
+        cin>>q;
+        while(q--){
+            long long x,y;
+            cin>>x>>y;
+            long long tat=x+y;
+            vector<long long>::iterator low1;
+            low1 =upper_bound(arr.begin(), arr.end(), tat);
+            if(arr[low1-arr.begin()-1]==tat){
+                cout<<-1<<endl;
+                
             }
-        }
-        if(ans==1){
-            cout<<"NO"<<endl;
-        }
-        else{
-            cout<<"YES"<<endl;
+            else{
+                
+                cout<<low1-arr.begin()<<endl;
+            
+            }
+
         }
         
 
